067d7eee8f072bf0e6f2ec049e6d3c435a6f5b22
C. Guevara Micciollo <c.guevara.micciollo@gmail.com>
2025-03-27 23:15:13 +0000

    mobile view done html and css static

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 "use strict";
+// menu of view in phone which appears when the user clicks on the icon of the menu.
+// DOM:
+const menuNavegation = document.getElementById('menuDesplegable');
+const menuIcono = document.getElementById('iconoMenu');
+// LOGIC:
+menuIcono.addEventListener('click', () => {
+    menuNavegation.style.display = 'flex';
+});
+menuNavegation.addEventListener('click', () => {
+    menuNavegation.style.display = 'none';
+});
